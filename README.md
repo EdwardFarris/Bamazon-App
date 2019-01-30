e5f39058cfd0d69ff5eaab0049a1411c47fbecaf
Edward Adam Farris <edwardadamfarris@Edwards-MacBook-Pro.local>
2019-01-30 13:43:10 -0700

    got file to communicate with database

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Bamazon-App
-A Node app similar to amazon in the way of browsing available inventory and selecting products for purchase through the command line
+### **Description**:
+A Node app similar to amazon in the way of browsing available inventory and selecting products for purchase through the command line. Users can specify a certain quantity of a product, unless that value is greater than the quantity in stock. Once a purchase is made, the datatable is updated to reflect the new quantity, and informs the user of their total cost.<br>
+
+### **Before You Begin**
+* Open Terminal or Gitbash
+* Clone Repo
+* within the directory of the repo, install Node packages:
+    * npm install mysql
+    * npm install inquirer<br>
+
+### **What the App Does**
+When this application is run through the terminal command line, it will do the following:
+1. Prints all available items from the store along with other information including the price, quantity, and department.
+2. Prompts the customer to choose an item to purchase by entering the item's unique ID number.
+3. Then prompts the customer to enter a valid quantity of the item if they want more than one.
+    * If there is enough of the item in stock, it will display the total cost to the customer.
+    * If there is NOT enough of the item in stock, it will inform the customer of this.
+    * If the purchase went through, the stock-quantity is immediately updated in the database to reflect the purchase.
+    * It also updates the total sales in the departments table.<br>
+
+### **Demo Videos**
+* bamazonCustomer.js (URL)
+
+### **Technologies Used
+* Node.js
+* MySQL 
+* JavaScript
+* Inquirer NPM package
+* mysql NPM package<br>
+
+### **Built With**
+* Visual Studio Code
+* MySQL Workbench
+* Terminal/Gitbash
+
+
+
+
+
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,171 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "LukeNLeia007!",
+    database: "bamazon"
+});
+
+function start() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("\nWelcome to Bamazon, Here are all our available products...\n");
+        console.log('------------------------------------------------------------------------------');
+
+        // Log all results of the SELECT statement
+        for(var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department Name: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY Available: " + res[i].stock_quantity);
+            console.log('-----------------------------------------------------------------------------------');
+        }
+
+        console.log(' ');
+        inquirer
+            .prompt([
+
+                {
+                    type: "input",
+                    name: "purchaseID",
+                    message: "What is the ID number of the product you wish to purchase?",
+                    validate: function (value) {
+                        if (isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "purchaseQTY",
+                    message: "How many of these items would you like to purchase!",
+                    validate: function (value) {
+                        if (isNaN(value)) {
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+            ]).then(function (answer) {
+                var itemToBuy = (answer.purchaseID) - 1;
+                var qtyToBuy = parseInt(answer.purchaseQTY);
+                var totalCost = parseFloat(((res[itemToBuy].price) * qtyToBuy).toFixed(2));
+
+                //check if quantity is sufficient
+                if (res[itemToBuy].stock_quantity >= qtyToBuy) {
+                    //update qty in products table
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: (res[itemToBuy].stock_quantity - qtyToBuy)
+                            },
+                            {
+                                item_id: answer.purchaseID
+                            }
+                        ],
+                        function (err, result) {
+                            if (err) throw err;
+                            console.log("Success! Your total is $" + totalCost.toFixed(2) + ". Your item(s) will be shipped in 3-5 business days.");
+                        });
+                    connection.query("SELECT * FROM departments", function (err, deptRes) {
+                        if (err) throw err;
+                        var index;
+                        for (var i = 0; i < deptRes.length; i++) {
+                            if (deptRes[i].Department_Name === res[itemToBuy].Department_Name) {
+                                index = i;
+                            }
+                        }
+                        //update total sales in departments table
+                        connection.query("UPDATE departments SET ? WHERE ?",
+                            [
+                                {
+                                    Total_Sales: deptRes[index].Total_Sales + totalCost
+                                },
+                                {
+                                    Department_Name: res[itemToBuy].Department_Name
+                                }
+                            ],
+                            function (err, deptRes) {
+                                if (err) throw err;
+                                console.log("Updated Department Sales.");
+                            });
+                    });
+                } else {
+                    console.log("Sorry, insufficient stock for that item! Please choose another quantity")
+                }
+                altprompt();
+            })
+    })
+}
+
+//Asks if they would like to make another purchase
+function altprompt() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "reply",
+            message: "Would you like to purchase another item?"
+        }]).then(function (answer) {
+            if (answer.reply) {
+                start();
+            } else {
+                console.log("Thanks for choosing Bamazon, hope to see you soon!");
+            }
+        });
+}
+start();
+
+
+
+// .then(function (inquirerResponse) {
+//     // If the inquirerResponse item is in-stock, inform customer of successfull transaction, otherwise inform of insufficient quantity!
+//     if (inquirerResponse.purchasQTY <= stock_quantity) {
+//         console.log("\nYour order has been processed for item number " + inquirerResponse.purchaseID);
+//         console.log("\nYour total is " + products.price + "!\n");
+//     }
+//     else {
+//         console.log("\nSorry, Insufficient quantity! Please try again!\n");
+//     }
+// });
+
+// //   function updateProduct() {
+// //         console.log("Updating stock quantities...\n");
+// //         var query = connection.query(
+// //           "UPDATE products SET ? WHERE ?",
+// //           [
+// //             {
+// //               stock_quantity: 100
+// //             },
+// //             {
+// //               item_id: ""
+// //             }
+// //           ],
+// //           function(err, res) {
+// //             console.log(res.affectedRows + " products updated!\n");
+// //             // Call deleteProduct AFTER the UPDATE completes
+// //             deleteProduct();
+// //           }
+// //         );
+
+// //         function deleteProduct() {
+// //             console.log("Deleting purchased Items...\n");
+// //             connection.query(
+// //               "DELETE FROM products WHERE ?",
+// //               {
+// //                 flavor: "strawberry"
+// //               },
+// //               function(err, res) {
+// //                 console.log(res.affectedRows + " products deleted!\n");
+// //                 // Call readProducts AFTER the DELETE completes
+// //                 readProducts();
+// //               }
+// //             )};
